484d053bbb55f90e746bfac996de146eb1ee2beb
jtrue <jtruex@gmail.com>
2019-01-05 05:19:29 -0500

    Initial API framework for pullItem method (APIWrapper Class)

--- app/src/main/java/com/unterr/truex/scapegoat/methods/APIWrapper.java
+++ app/src/main/java/com/unterr/truex/scapegoat/methods/APIWrapper.java
@@ -1,8 +1,21 @@
 package com.unterr.truex.scapegoat.methods;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
 
 import  com.unterr.truex.scapegoat.models.Item;
 import  com.unterr.truex.scapegoat.models.Player;
+
+//Import statements to handle JSON data and JSON exceptions
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /*
  * APIWrapper:
@@ -21,6 +34,7 @@ public class APIWrapper {
     public static Player pullPlayer( String username ){
         Player newPlayerObject = new Player();
 
+
         //TODO: pull the herblore lvl from the API and return it
 
         return newPlayerObject;
@@ -28,6 +42,102 @@ public class APIWrapper {
 
     public static Item pullItem( Double itemID ){
         Item newItemObject = new Item();
+
+        class DownloadTask extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... urls) {
+
+                String result = "";
+                URL url;
+                HttpURLConnection urlConnection = null;
+
+                try {
+                    url = new URL(urls[0]);
+
+                    urlConnection = (HttpURLConnection) url.openConnection();
+
+                    InputStream in = urlConnection.getInputStream();
+
+                    InputStreamReader reader = new InputStreamReader(in);
+
+                    int data = reader.read();
+
+                    while (data != -1) {
+
+                        char current = (char) data;
+
+                        result += current;
+
+                        data = reader.read();
+
+                    }
+
+                    return result;
+
+                } catch (Exception e) {
+
+
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+
+                try {
+
+                    String message = "";
+
+                    JSONObject jsonObject = new JSONObject(result);
+
+                    String itemInfo = jsonObject.getString("item");
+
+                    Log.i("Item Data", itemInfo);
+
+                    JSONArray arr = new JSONArray(itemInfo);
+
+                    for (int i = 0; i < arr.length(); i++) {
+
+                        JSONObject jsonPart = arr.getJSONObject(i);
+
+                        String main = "";
+                        String description = "";
+
+                        main = jsonPart.getString("current");
+                        description = jsonPart.getString("description");
+
+                        if (main != "" && description != "") {
+
+                            message += main + ": " + description + "\r\n";
+
+                        }
+
+                    }
+
+                    if (message != "") {
+
+
+
+                    } else {
+
+                    }
+
+
+                } catch (JSONException e) {
+
+
+                }
+
+
+
+            }
+        }
+
+        DownloadTask task = new DownloadTask();
+        task.execute("http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=" + itemID.toString());
 
         //TODO: pull item info from the API and return
 
